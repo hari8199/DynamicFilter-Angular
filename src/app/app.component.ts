1ada81b6d43e05da2f479787c435912498cfac68
hari8199 <hari.hasini3@gmail.com>
2019-07-07 22:31:16 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Project';
+  
+  private loggedIn: boolean=false;
+
+  constructor() { }
+
+  ngOnInit() {
+   
+  }
+}
